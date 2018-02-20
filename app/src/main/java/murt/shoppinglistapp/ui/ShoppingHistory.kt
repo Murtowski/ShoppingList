6b23ee55ebe29c509d031aa1b0608ce4bbed7a81
Piotr Piskorski <piskorski.pio@gmail.com>
2018-02-20 11:03:33 +0100

    Add Shopping History Activity

---
+++ app/src/main/java/murt/shoppinglistapp/ui/ShoppingHistory.kt
@@ -0,0 +1,13 @@
+package murt.shoppinglistapp.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import murt.shoppinglistapp.R
+
+class ShoppingHistory : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping_history)
+    }
+}
