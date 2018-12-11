d0ade04547f11cb6dd3066deeb7321b27fdbff56
Piotr Piskorski <piotr.piskorski@etteplan.com>
2018-12-11 14:46:03 +0100

    change RecyclerView to to ListAdapter, add view binding to example shopping)item

--- app/src/main/java/murt/shoppinglistapp/ui/shoppingListDetails/ShoppingListExampleAdapter.kt
+++ app/src/main/java/murt/shoppinglistapp/ui/shoppingListDetails/ShoppingListExampleAdapter.kt
@@ -4,6 +4,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_shopping.view.*
 import murt.data.model.ShoppingItem
@@ -12,6 +13,55 @@ import murt.shoppinglistapp.ui.utils.getReadableDate
 import murt.shoppinglistapp.ui.utils.inflate
 
 
+
+class ShoppingListExampleAdapter2(
+        val saveItem: (ShoppingItem) -> Unit
+): ListAdapter<ShoppingItem, ShoppingListExampleAdapter2.ShoppingItemExampleViewHolder>(ShoppingItemExampleDiffUtils()) {
+
+    fun updateList(newShoppingList: List<ShoppingItem>){
+        submitList(newShoppingList)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingItemExampleViewHolder {
+        return ShoppingItemExampleViewHolder(parent.inflate(R.layout.item_shopping_example))
+    }
+
+    override fun onBindViewHolder(holder: ShoppingItemExampleViewHolder, position: Int) {
+        holder.onBindEdit(getItem(position))
+    }
+
+    inner class ShoppingItemExampleViewHolder(val view: View): RecyclerView.ViewHolder(view){
+
+        private val date = view.item_shopping_date
+        private val title = view.item_shopping_title
+
+        fun onBindEdit(item: ShoppingItem){
+
+            date.text = item.updatedAt.getReadableDate(date.context)
+            title.text = item.title
+
+            view.setOnClickListener {
+                saveItem(getItem(adapterPosition))
+            }
+        }
+
+    }
+
+    class ShoppingItemExampleDiffUtils: DiffUtil.ItemCallback<ShoppingItem>(){
+
+        override fun areContentsTheSame(oldItem: ShoppingItem, newItem: ShoppingItem): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areItemsTheSame(oldItem: ShoppingItem, newItem: ShoppingItem): Boolean {
+            return oldItem.title == newItem.title
+        }
+    }
+}
+
+interface ClickListener{
+    fun onItemClick(item: ShoppingItem)
+}
 
 class ShoppingListExampleAdapter(
         val saveItem: (ShoppingItem) -> Unit
